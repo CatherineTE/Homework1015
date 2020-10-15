dfec83320835471520161c1d41c4490632f81a27
CatherineTE <72939295+CatherineTE@users.noreply.github.com>
2020-10-15 22:09:42 +0300

    Add files via upload

---
+++ src/com/catherineTE/testproject/Displayer.java
@@ -0,0 +1,15 @@
+package com.catherineTE.testproject;
+
+public class Displayer {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello Word!");
+	    String text = "Be Happy!";
+	    System.out.println(text);
+        showInConsole("Wonderful Life!");
+    }
+
+    public static void showInConsole (String t) {
+        System.out.println(t);
+    }
+}
